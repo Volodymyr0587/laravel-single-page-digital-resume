b8550a16d1c391f0c445dd5573fc3d64607ecfcd
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-06-22 15:29:10 +0300

    caching

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers;
 
 use App\DataObjects\Resume;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 
 class ResumeController extends Controller
 {
     public function index()
     {
-        $resume = Storage::disk('resumes')->get('resume.json');
-        $resumeData = json_decode($resume, true);
+        $resume = Cache::remember('resume_data', now()->addDay(), function () {
+            $resume = Storage::disk('resumes')->get('resume.json');
+            $resumeData = json_decode($resume, true);
 
-        return view('resume', ['resume' => Resume::fromArray($resumeData), 'allowDownload' => true]);
+            return Resume::fromArray($resumeData);
+        });
+
+        return view('resume', ['resume' => $resume, 'allowDownload' => true]);
     }
 }
