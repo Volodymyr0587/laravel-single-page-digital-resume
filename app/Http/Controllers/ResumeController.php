eac14b5a0bce2d6e2c5f89f0430a40308f15297e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2025-06-22 15:43:31 +0300

    Generating PDF

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\DataObjects\Resume;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
@@ -19,5 +20,19 @@ class ResumeController extends Controller
         });
 
         return view('resume', ['resume' => $resume, 'allowDownload' => true]);
+    }
+
+    public function download()
+    {
+        $resume = Cache::remember('resume_data', now()->addDay(), function () {
+            $resume = Storage::disk('resumes')->get('resume.json');
+            $resumeData = json_decode($resume, true);
+
+            return Resume::fromArray($resumeData);
+        });
+
+        $pdf = Pdf::loadView('resume', ['resume' => $resume, 'allowDownload' => true]);
+
+        return $pdf->download($resume->basics->name . ' Resume.pdf');
     }
 }
